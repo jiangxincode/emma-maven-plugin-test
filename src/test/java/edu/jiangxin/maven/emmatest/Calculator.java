d2374fa281fbccd5554ab2859cb9440b4d61e78e
Aloys <jiangxinnju@163.com>
2022-03-20 13:05:16 +0800

    init

---
+++ src/test/java/edu/jiangxin/maven/emmatest/Calculator.java
@@ -0,0 +1,24 @@
+package edu.jiangxin.maven.emmatest;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) throws Exception {
+		if (0 == b) {
+			throw new Exception("除数不能为0");
+		}
+
+		return a / b;
+	}
+
+}
